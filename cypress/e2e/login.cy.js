95721a6d4e1858187da4f6fd5f018ae1d12569c2
brunosouza <brunobbbsss@gmail.com>
2026-02-18 07:22:31 -0300

    Added selections Spic

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,25 @@
 describe('Orange Test', () => {
+  const selectorslist = {
+    usernameField: '[name="username"]',
+    passwordField: '[name="password"]',
+    submitButton: '[type="submit"]',
+    sessionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    wrongCredentialsAlert: '.oxd-alert'
+  }
+
   it('Login Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('[type="submit"]').click()
+    cy.get(selectorslist.usernameField).type('Admin')
+    cy.get(selectorslist.passwordField).type('admin123')
+    cy.get(selectorslist.submitButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorslist.sessionTitleTopBar).contains('Dashboard')
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Test')
-    cy.get('[name="password"]').type('test')
-    cy.get('[type="submit"]').click()
-    cy.get('.oxd-alert')
+    cy.get(selectorslist.usernameField).type('Test')
+    cy.get(selectorslist.passwordField).type('test')
+    cy.get(selectorslist.submitButton).click()
+    cy.get(selectorslist.wrongCredentialsAlert)
   })
 })
