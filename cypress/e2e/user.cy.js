fb161a495f28e1c4601739ca14a0f16a55f4d2d5
brunosouza <brunobbbsss@gmail.com>
2026-02-25 01:06:17 -0300

    implement Page Object Model to improve test maintainability

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,14 +1,15 @@
 import userData from '../fixtures/UserData.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPages from '../pages/menuPages.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPages = new MenuPages()
 
 describe('Orange Test', () => {
   const selectorslist = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    submitButton: '[type="submit"]',
-    sessionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashbordGrid: '.orangehrm-dashboard-grid',
-    wrongCredentialsAlert: '.oxd-alert',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    
     personalDetailMyInfo: '[href="/web/index.php/pim/viewPersonalDetails/empNumber/7"]',
     infoPersonalDetails: '.oxd-input--active',
     licenseExpiryDate: '[placeholder="yyyy-dd-mm"]',
@@ -17,24 +18,22 @@ describe('Orange Test', () => {
   }  
  
   it.only('User Info Update', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorslist.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorslist.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorslist.submitButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorslist.dashbordGrid)
-    cy.get(selectorslist.myInfoButton).click()
+    loginPage.acessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.checkDashboardPage()
+    menuPages.accessMyInfo()
+  /* 
     cy.get(selectorslist.personalDetailMyInfo).click()
     cy.get('[name="firstName"]').clear().type('Allan')
     cy.get('[name="middleName"]').clear().type('Borges')
     cy.get('[name="lastName"]').clear().type('Silva')
- // cy.get(selectorslist.infoPersonalDetails).eq(3).clear().type('Alô Borginhos')
+  //  cy.get(selectorslist.infoPersonalDetails).eq(3).clear().type('Alô Borginhos')
     cy.get(selectorslist.infoPersonalDetails).eq(3).clear().type('Empl007')
     cy.get(selectorslist.infoPersonalDetails).eq(4).clear().type('Other007')
     cy.get(selectorslist.infoPersonalDetails).eq(5).clear().type('Drive007')
     cy.get(selectorslist.licenseExpiryDate).eq(0).clear().type('2030-09-20')
     cy.get(selectorslist.dateCloseButton).click()
-    cy.get(selectorslist.licenseExpiryDate).eq(1).clear().type('1930-09-07')
+    cy.get(selectorslist.licenseExpiryDate).eq(1).clear().type('1945-09-07')
     cy.get(selectorslist.dateCloseButton).click()
     cy.get('.oxd-select-text').eq(0).click()
     cy.get('.oxd-select-wrapper').contains('Belgian').click()
@@ -47,7 +46,7 @@ describe('Orange Test', () => {
     cy.get('.oxd-select-wrapper').contains('O+').click()
     cy.get("[options='']").clear().type('Deu certo')
     cy.get("[type='submit']").eq(1).click()
-    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('body').should('contain', 'Successfully Updated') */
       
   })
   it('Login - Fail', () => {
